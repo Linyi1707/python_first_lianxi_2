884156074fcfffd6e9c8cc9f39b67d8517e765c0
Linyi1707 <117964958+linyi1707@users.noreply.github.com>
2023-02-08 16:07:53 +0800

    修正两处bug

--- 彩票模拟.py
+++ 彩票模拟.py
@@ -1,5 +1,4 @@
 # 彩票模拟小游戏
-# 彩票模拟小游戏
 import random
 k1 = '开始游戏'
 k2 = '结束游戏'
@@ -10,39 +9,40 @@ try:
 	while 1:
 		data = list()
 		for i in range(0, 6):
-			 = random.choice(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
-			data.append()
+			s = random.choice(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
+			data.append(s)
 		while 1:
 			data1 = money
 			if money <= 0:
 				money = int(input('余额过少，请充值：')) + money
 			else:
-				jiang = int(input('请输入想要投入的金额：'))
+				data3 = jiang = int(input('请输入想要投入的金额：'))
 				money = money - jiang
-				jiang = data2 = jiang * 2 / 3
+				jiang = data2 = jiang * 2 // 3
 				print('您现在的余额为%d.' % money)
 				break
+		if jiang < 0:
+			money = money + data3
+			break
 		if money < 0:
 			print('请输入小于余额的金额！')
 			money = data1
 			break
 		cai = list(input('请输入你的竞猜号码：'))
 		a = int()
+		x = int(2)
 		for i in range(0, len(data)):
 			if cai[i] == data[i]:
 				a = a + 1
-			for b in range(1, a):
-				print(b)
-				data2 = data2 * (2 * b)
-				break
+		for b in range(1, a):
+			data2 = data2 * x
 		if a == 0:
 			jiang = 0
 		else:
 			jiang = data2
 		money = jiang + money
 		print('中奖号码为\n%s\n您猜中了%d个号码，奖金为%d，奖金已为您放入余额。\n您的余额为%d' % (data, a, jiang, money))
-		con = input("请问还要继续竞猜么？如果不想请请按1,继续请任意输入字符：")
-		if con == "1":
+		if str(1) == input("请问还要继续竞猜么？如果不想请请按1,继续请任意输入字符："):
 			break
 except (ValueError, IndexError):
 	print('请输入正确字符！')
